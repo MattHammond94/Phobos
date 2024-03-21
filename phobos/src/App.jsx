33650553ad41e59163d828a3bd7cba42ebfd5ccd
Matt Hammond <mjhammond94@gmail.com>
2024-03-21 11:06:29 +0000

    Install dependencies and Add fact generation logic to App

--- phobos/src/App.jsx
+++ phobos/src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [fact, setFact] = useState("");
+
+  const generateFact = async () => {
+    console.log('API called');
+    console.log(import.meta.env.VITE_OPEN_AI_API_KEY);
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: 'Generate a randomised fact about the planet Mars. If you have told me this fact before please generate a new different one' }],
+        temperature: 0.7
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${import.meta.env.VITE_OPEN_AI_API_KEY}`
+        }
+      });
+
+      setFact(response.data.choices[0].message.content);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 
   return (
-    <>
+    <div>
       <h1>Phobos</h1>
-      <h2>Phobos</h2>
-      <h3>Phobos</h3>
-    </>
+      <button onClick={ generateFact }>Generate a fact about Mars</button>
+      {fact && <p>{ fact }</p>}
+    </div>
   )
 }
 
